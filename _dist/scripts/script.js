ea4ea87c429e7ed788d602630e3e1adfc972176c
Rizwan Memon <rizwan2000.rm@gmail.com>
2020-06-10 07:04:33 +0530

    working v2

--- _dist/scripts/script.js
+++ _dist/scripts/script.js
@@ -6,7 +6,12 @@ const dateDisplay = document.querySelector(".date");
 const highTempDisplay = document.querySelector("#high-temp");
 const lowTempDisplay = document.querySelector("#low-temp");
 const windSpeedDisplay = document.querySelector("#wind-speed");
-const windDiretionDisplay = document.querySelector(".wind-arrow");
+const windDirectionDisplay = document.querySelector("#wind-direction");
+
+const previousSolTemplate = document.querySelector(
+  "[data-previous-sol-template]"
+);
+const previousSolContainer = document.querySelector("[data-previous-sols]");
 
 let recentSolIndex;
 
@@ -18,6 +23,7 @@ getWeather().then((sols) => {
   recentSolIndex = sols.length - 1;
   console.log(sols[recentSolIndex]);
   displayRecentSol(sols);
+  displayPreviousSols(sols);
 });
 
 function displayRecentSol(sols) {
@@ -27,10 +33,24 @@ function displayRecentSol(sols) {
   highTempDisplay.textContent = recentSol.maxTemp;
   lowTempDisplay.textContent = recentSol.minTemp;
   windSpeedDisplay.textContent = recentSol.windSpeed;
-  windDiretionDisplay.style.setProperty(
-    "--direction",
-    `${recentSol.windDirectionDegrees}deg`
-  );
+  windDirectionDisplay.textContent = recentSol.windDirectionDegrees;
+}
+
+function displayPreviousSols(sols) {
+  previousSolContainer.innerHTML = "";
+  sols.forEach((solData, index) => {
+    if (solData != sols[recentSolIndex]) {
+      const solContainer = previousSolTemplate.content.cloneNode(true);
+      solContainer.querySelector("[data-sol]").innerText = solData.sol;
+      solContainer.querySelector("[data-date]").innerText = displayDate(
+        solData.date
+      );
+      solContainer.querySelector("[data-temp-high]").innerText =
+        solData.maxTemp;
+      solContainer.querySelector("[data-temp-low]").innerText = solData.minTemp;
+      previousSolContainer.appendChild(solContainer);
+    }
+  });
 }
 
 function getWeather() {
@@ -41,12 +61,12 @@ function getWeather() {
       return Object.entries(solData).map(([sol, data]) => {
         return {
           sol: sol,
+          date: new Date(data.First_UTC),
           maxTemp: data.AT.mx,
           minTemp: data.AT.mn,
           windSpeed: data.HWS.av,
-          windDirectionDegrees: data.WD.most_common.compass_degrees,
           windDirectionCardinal: data.WD.most_common.compass_point,
-          date: new Date(data.First_UTC),
+          windDirectionDegrees: data.WD.most_common.compass_degrees
         };
       });
     });
